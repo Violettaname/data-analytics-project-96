100821a308e1dcb4ea91b8b4c2992e8474c37abe
Violettaname <vita.vendetta@mail.ru>
2025-06-04 15:50:00 +0300

    Update dashboard.sql

--- dashboard.sql
+++ dashboard.sql
@@ -1,4 +1,5 @@
-/* Расчеты метрик произведены на основе таблицы aggregate_last_paid_click, построенной по модели атрибуции Last Paid Click */
+/* Расчеты метрик произведены на основе таблицы aggregate_last_paid_click, 
+построенной по модели атрибуции Last Paid Click */
 WITH tab AS (
     SELECT
         visitor_id,
@@ -58,27 +59,25 @@ ads AS (
     GROUP BY 1, 2, 3, 4
 ),
 
-aggregate_last_paid_click as (	
-SELECT
-    t2.visit_date,
-    t2.visitors_count,
-    t2.utm_source,
-    t2.utm_medium,
-    t2.utm_campaign,
-    a.total_cost,
-    t2.leads_count,
-    t2.purchases_count,
-    t2.revenue
-FROM tab2 AS t2
-LEFT JOIN ads AS a
-    ON
-        t2.visit_date = a.campaign_date
+aggregate_last_paid_click AS (
+  SELECT
+	t2.visit_date,
+        t2.visitors_count,
+        t2.utm_source,
+        t2.utm_medium,
+        t2.utm_campaign,
+        a.total_cost,
+        t2.leads_count,
+        t2.purchases_count,
+        t2.revenue
+ FROM tab2 AS t2
+ LEFT JOIN ads AS a
+	ON t2.visit_date = a.campaign_date
         AND t2.utm_source = a.utm_source
         AND t2.utm_medium = a.utm_medium
         AND t2.utm_campaign = a.utm_campaign
 ORDER BY 9 DESC NULLS LAST, 1 ASC, 5 DESC, 2, 3, 4
 )
-	
 /* количество посетителей, лидов, покупателей */
 SELECT
     sum(visitors_count) AS visitors,
@@ -95,7 +94,6 @@ SELECT
         sum(purchases_count) * 100.0 / sum(leads_count), 2
     ) AS conv_lead_to_purchase
 FROM aggregate_last_paid_click;
-
 /* выручка, затраты, CPU, CPL, CPPU, ROI */
 SELECT 
 	sum(revenue) AS revenue,
@@ -105,7 +103,6 @@ SELECT
 	round(sum(total_cost) / sum(purchases_count), 2) AS cppu
 	round((SUM(revenue)-SUM(total_cost))*100.0/(SUM(total_cost)), 2) AS roi,
 FROM aggregate_last_paid_click;
-
 /* динамика посещений */
 SELECT
     visit_date,
@@ -123,7 +120,6 @@ GROUP BY
         ELSE 'other'
     END
 ORDER BY visitors DESC
-
 /* источники трафика: распределение трафика по каналам */
 SELECT
     CASE
@@ -140,7 +136,6 @@ GROUP BY
         ELSE 'other'
     END
 ORDER BY visitors DESC
-
 /* количество лидов по каналам */
 SELECT
     CASE
@@ -157,7 +152,6 @@ GROUP BY
         ELSE 'other'
     END
 ORDER BY leads DESC
-
 /* количество посетителей, лидов, покупателей по каналам (воронка: клик --> лид --> оплата) */
 SELECT
     CASE
@@ -176,7 +170,6 @@ GROUP BY
         ELSE 'other'
     END
 ORDER BY visitors DESC
-
 /* Конверсии по каналам, % */
 SELECT
     CASE
@@ -211,7 +204,6 @@ select
     sum(va.daily_spent) as costs
 from vk_ads as va
 group by 1;
-
 /* окупаемость рекламных кампаний*/
 SELECT
     utm_campaign,
@@ -223,7 +215,6 @@ FROM aggregate_last_paid_click
 WHERE utm_source IN ('yandex', 'vk')
 GROUP BY utm_campaign, utm_source
 ORDER BY source DESC NULLS LAST
-
 /* итоговая таблица по агрегации source */
 SELECT
     utm_source AS source,
@@ -239,7 +230,6 @@ SELECT
 FROM aggregate_last_paid_click
 GROUP BY utm_source
 ORDER BY roi DESC NULLS LAST
-
 /* итоговая таблица по агрегации medium */
 SELECT
     utm_medium AS medium,
@@ -255,7 +245,6 @@ SELECT
 FROM aggregate_last_paid_click
 GROUP BY utm_medium
 ORDER BY roi DESC NULLS LAST;
-
 /* время закрытия лидов */
 WITH tab AS (
     SELECT
