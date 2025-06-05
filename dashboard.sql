8fd89d3144ac7e8cdcc2ae138954e70dfec449ef
Violettaname <vita.vendetta@mail.ru>
2025-06-05 13:08:26 +0300

    Update dashboard.sql

--- dashboard.sql
+++ dashboard.sql
@@ -99,11 +99,12 @@ FROM aggregate_last_paid_click;
 /* выручка, затраты, CPU, CPL, CPPU, ROI */
 SELECT
     SUM(revenue) AS revenue,
-    SUM(total_cost) AS costs,
+    SUM(total_cost) AS total_cost,
     ROUND(SUM(total_cost) / SUM(visitors_count), 2) AS cpu,
     ROUND(SUM(total_cost) / SUM(leads_count), 2) AS cpl,
     ROUND(SUM(total_cost) / SOM(purchases_count), 2) AS cppu,
-    ROUND((SUM(revenue) - SUM(total_cost)) * 100.0 / (SUM(total_cost)), 2) AS roi
+    ROUND((SUM(revenue) - SUM(total_cost)) * 100.0 /
+    (SUM(total_cost)), 2) AS roi
 FROM aggregate_last_paid_click;
 /* динамика посещений */
 SELECT
